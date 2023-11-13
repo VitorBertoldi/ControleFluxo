bd6da68095418f08c421280e2102cd65b034981b
VitorBertoldi <117694686+VitorBertoldi@users.noreply.github.com>
2023-11-13 16:22:06 -0300

    commit

---
+++ ControleFluxo/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a primeira nota: ");
+        double nota1 = sc.nextDouble();
+
+        System.out.println("Digite a segunda nota: ");
+        double nota2 = sc.nextDouble();
+
+        System.out.println("Digite a terceira nota: ");
+        double nota3 = sc.nextDouble();
+
+        System.out.println("Digite a quarta nota: ");
+        double nota4 = sc.nextDouble();
+
+        double media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+        if (media >= 7)
+            System.out.println("APROVADO");
+        else if (media >= 5)
+            System.out.println("RECUPERACAO");
+        else
+            System.out.println("REPROVADO");
+
+    }
+}
